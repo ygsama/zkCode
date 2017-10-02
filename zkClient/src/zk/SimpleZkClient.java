11cf2db4080621d2c27f3383ae306aff5a4597fc
ygsama <ygsama@qq.com>
2017-10-02 22:04:38 +0800

    java操作zkClient

---
+++ zkClient/src/zk/SimpleZkClient.java
@@ -0,0 +1,78 @@
+package zk;
+
+import java.util.List;
+
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooDefs.Ids;
+import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.data.Stat;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SimpleZkClient {
+
+	private static final String connStr = "106.14.149.247:2181,101.132.106.110:2181,116.196.88.198:2181";
+//	private static final String connStr = "master:2181,hadoop1:2181,hadoop2:2181";
+	private static final int sessionTimeout = 2000;
+	private static ZooKeeper client = null;
+
+	@Before
+	public void init() throws Exception{
+		client = new ZooKeeper(connStr, sessionTimeout, new Watcher() {
+					
+					@Override
+					public void process(WatchedEvent event) {
+						// 收到事件通知后的回调函数，事件处理逻辑
+						System.out.println(event.getType()+"\t"+event.getPath()+"\tconnected");
+					}
+				});
+	}
+	
+	/**
+	 *  数据增删改查 = 路径，数据，权限，类型
+	 */
+	@Test
+	public void testCreate() throws Exception{
+		
+		String nodePath = client.create("/eclipse", "hello zk".getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+		System.out.println(nodePath);
+	}
+	
+	@Test
+	public void getChildren() throws Exception {
+		
+		List<String> children = client.getChildren("/", true);
+		for (String child : children) {
+			System.out.println(child);
+		}
+	}
+	
+	@Test
+	public void isExist() throws Exception{
+
+		Stat stat = client.exists("/eclipse", false);
+		System.out.println(stat==null?"not exist":"exist and data length is " + stat.getDataLength());
+	}
+	
+	@Test
+	public void getData() throws Exception{
+
+		byte[] data = client.getData("/eclipse", false, null);
+		System.out.println(new String(data));
+	}
+	
+	@Test
+	public void deleteZndoe() throws Exception{
+		
+		client.delete("/eclipse", -1);
+	}
+	
+	@Test
+	public void resetZndoe() throws Exception{
+		
+		client.setData("/eclipse", "hello world".getBytes(), -1);
+		
+	}
+}
